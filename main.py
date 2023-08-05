47b8ec61fefb2929c0e8574c693c3c69b1f350f1
tondra93 <tondranahian@gmail.com>
2023-08-05 15:41:30 +0600

    fourth commit

--- main.py
+++ main.py
@@ -32,7 +32,12 @@ def check_all_messages(message):
     def response(bot_response,list_of_words, single_response=False, required_words=[]):
         nonlocal highest_prob_list
         highest_prob_list[bot_response]=message_probability(message,list_of_words,single_response,required_words)
-
+    response('Hello!',['hello','hi','hey','sup','heyo'],single_response=True)
+    response('See you!',['bye','goodbye'],single_response=True)
+    response('I\'m doing fine, and you?',['how','are','you','doing'],required_words=['how'])
+    response('You\'re welcome!',['thank','thanks'],single_response=True)
+    best_match=max(highest_prob_list,key=highest_prob_list.get)
+    print(highest_prob_list)
 def get_response(user_input):
     split_message=re.split(r'\s+|[,;?!.-]\s*',user_input.lower())
     response=check_all_messages(split_message)
